40df9fa10d1622c62a1cf5e04a97bf708237cfbc
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2021-10-13 17:31:35 +0200

    new scripts and stuff

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Input : MonoBehaviour
+{
+    [HideInInspector] public Vector2 moveVector;
+    [HideInInspector] public bool dash;
+    [HideInInspector] public bool interact;
+    [HideInInspector] public bool grapple;
+    //[HideInInspector] public bool quit;
+    [HideInInspector] public bool restart;
+    
+    private void Update()
+    {
+        moveVector.x = (Keyboard.current.aKey.isPressed ? -1f : 0f) + (Keyboard.current.dKey.isPressed ? 1f : 0f);
+        moveVector.y = (Keyboard.current.sKey.isPressed ? -1f : 0f) + (Keyboard.current.wKey.isPressed ? 1f : 0f);
+        
+        if (moveVector.magnitude > 1)
+        {
+            moveVector = moveVector.normalized;
+        }
+        
+        dash = Keyboard.current.spaceKey.wasPressedThisFrame;
+        interact = Keyboard.current.fKey.wasPressedThisFrame;
+        attack = Keyboard.current.kKey.wasPressedThisFrame;
+
+        quit = Keyboard.current.escapeKey.wasPressedThisFrame;
+        restart = Keyboard.current.rKey.wasPressedThisFrame;
+    }
+}
