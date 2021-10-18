f1247e814e1746f33a02c0614c50bd179c7ad855
TheSaltyGaming <38183890+TheSaltyGaming@users.noreply.github.com>
2021-10-18 11:45:26 +0200

    Added very experimental movement script

--- Assets/Scripts/TestMovement.cs
+++ Assets/Scripts/TestMovement.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(PlayerStateList))]
+[RequireComponent(typeof(Input))]
 public class TestMovement : MonoBehaviour
 {
+    public PlayerStateList pState;
+    
     [Header("X Axis Movement")]
     [SerializeField] float walkSpeed = 25f;
  
@@ -14,7 +18,210 @@ public class TestMovement : MonoBehaviour
     [SerializeField] float fallSpeed = 45;
     [SerializeField] int jumpSteps = 20;
     [SerializeField] int jumpThreshold = 7;
-    [Space(5)] 
+    [Space(5)]
     
-    private bool test;
+    [Header("Recoil")]
+    [SerializeField] int recoilXSteps = 4;
+    [SerializeField] int recoilYSteps = 10;
+    [SerializeField] float recoilXSpeed = 45;
+    [SerializeField] float recoilYSpeed = 45;
+    [Space(5)]
+    
+    
+    float xAxis;
+    float yAxis;
+    int stepsXRecoiled;
+    int stepsYRecoiled;
+    int stepsJumped = 0;
+    float grabity;
+    Rigidbody2D rb;
+
+    private Input _input;
+    private Collision _collision;
+    
+    void Start () {
+        if(pState == null)
+        {
+            pState = GetComponent<PlayerStateList>();
+        }
+ 
+        rb = GetComponent<Rigidbody2D>();
+        _input = GetComponent<Input>();
+        _collision = GetComponent<Collision>();
+ 
+        grabity = rb.gravityScale;
+    }
+    
+    void Update () {
+        Flip();
+        Walk(xAxis);
+        Recoil();
+        GetInputs();
+    }
+    
+    void FixedUpdate()
+    {
+        if (pState.recoilingX == true && stepsXRecoiled < recoilXSteps)
+        {
+            stepsXRecoiled++;
+        }
+        else
+        {
+            StopRecoilX();
+        }
+        if (pState.recoilingY == true && stepsYRecoiled < recoilYSteps)
+        {
+            stepsYRecoiled++;
+        }
+        else
+        {
+            StopRecoilY();
+        }
+        if (_collision.IsGrounded())
+        {
+            StopRecoilY();
+        }
+ 
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (pState.jumping)
+        {
+
+            if (stepsJumped < jumpSteps)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+                stepsJumped++;
+            }
+            else
+            {
+                StopJumpSlow();
+            }
+        }
+    }
+    
+    void Walk(float MoveDirection)
+    {
+        //Rigidbody2D rigidbody2D = rb;
+        //float x = MoveDirection * walkSpeed;
+        //Vector2 velocity = rb.velocity;
+        //rigidbody2D.velocity = new Vector2(x, velocity.y);
+        if (!pState.recoilingX)
+        {
+            rb.velocity = new Vector2(MoveDirection * walkSpeed, rb.velocity.y);
+ 
+            if (Mathf.Abs(rb.velocity.x) > 0)
+            {
+                pState.walking = true;
+            }
+            else
+            {
+                pState.walking = false;
+            }
+            if (xAxis > 0)
+            {
+                pState.lookingRight = true;
+            }
+            else if (xAxis < 0)
+            {
+                pState.lookingRight = false;
+            }
+ 
+            //anim.SetBool("Walking", pState.walking);
+        }
+ 
+    }
+    
+    void Recoil()
+    {
+        //since this is run after Walk, it takes priority, and effects momentum properly.
+        if (pState.recoilingX)
+        {
+            if (pState.lookingRight)
+            {
+                rb.velocity = new Vector2(-recoilXSpeed, 0);
+            }
+            else
+            {
+                rb.velocity = new Vector2(recoilXSpeed, 0);
+            }
+        }
+        if (pState.recoilingY)
+        {
+            if (yAxis < 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, recoilYSpeed);
+                rb.gravityScale = 0;
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -recoilYSpeed);
+                rb.gravityScale = 0;
+            }
+ 
+        }
+        else
+        {
+            rb.gravityScale = grabity;
+        }
+    }
+ 
+    void Flip()
+    {
+        if (xAxis > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
+        else if (xAxis < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+    }
+    
+    void StopJumpQuick()
+    {
+        //Stops The player jump immediately, causing them to start falling as soon as the button is released.
+        stepsJumped = 0;
+        pState.jumping = false;
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+    }
+ 
+    void StopJumpSlow()
+    {
+        //stops the jump but lets the player hang in the air for awhile.
+        stepsJumped = 0;
+        pState.jumping = false;
+    }
+ 
+    void StopRecoilX()
+    {
+        stepsXRecoiled = 0;
+        pState.recoilingX = false;
+    }
+ 
+    void StopRecoilY()
+    {
+        stepsYRecoiled = 0;
+        pState.recoilingY = false;
+    }
+
+    void GetInputs()
+    {
+        if (_input.jump && _collision.IsGrounded())
+        {
+            pState.jumping = true;
+        }
+
+        if (_input.jump && stepsJumped < jumpSteps && stepsJumped > jumpThreshold && pState.jumping)
+        {
+            StopJumpQuick();
+        }
+        else if (_input.jump && stepsJumped < jumpThreshold && pState.jumping)
+        {
+            StopJumpSlow();
+        }
+    }
+    
 }
